3daf7fdaf95f85444d4c54ccc933408f33787578
Chung <cn268471@gmail.com>
2026-01-31 15:27:19 +0700

    code 15:26 31/1/2026

--- script.js
+++ script.js
@@ -8,21 +8,22 @@ function random(min, max) {
   return Math.random() * (max - min) + min;
 }
 class Particle {
-  constructor(x, y, color) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.color = color;
-    this.radius = 2;
+    this.color = `hsl(${random(0, 360)}, 100%, 75%)`;
+    this.radius = 1.3;
 
     const goc = Math.random() * Math.PI * 2;
-    const vantoc = Math.random() * 5;
+    const vantoc = Math.random() * 3;
     this.vantocx = Math.cos(goc) * vantoc;
     this.vantocy = Math.sin(goc) * vantoc;
-    const life = 10;
+    this.life = 60;
   }
   update() {
     this.x = this.x + this.vantocx;
     this.y = this.y + this.vantocy;
+    this.vantocy = this.vantocy + 0.01;
     this.life--;
   }
   draw() {
@@ -33,18 +34,31 @@ class Particle {
   }
 }
 
+let particles = [];
+
+function exploide(x, y, color) {
+  for (let i = 0; i < 50; i++) {
+    particles.push(new Particle(x, y, color));
+  }
+}
+
 class Firework {
   constructor() {
     this.x = random(0, canvas.width);
     this.y = canvas.height;
     this.targetY = random(100, canvas.height / 2);
     this.radius = 2;
-    this.color = `hsl(${random(0, 360)}, 100%, 60%)`;
+    this.color = `hsl(${random(0, 360)}, 100%, 75%)`;
     this.speed = random(4, 7);
+    this.exploded = false;
   }
 
   update() {
     this.y -= this.speed;
+    if (this.y <= this.targetY && !this.exploded) {
+      this.exploded = true;
+      exploide(this.x, this.y, this.color);
+    }
   }
 
   draw() {
@@ -69,8 +83,15 @@ function animate() {
     f.update();
     f.draw();
 
-    if (f.y < f.targetY) {
+    if (f.exploded) {
       fireworks.splice(i, 1);
+    }
+  });
+  particles.forEach((p, i) => {
+    p.update();
+    p.draw();
+    if (p.life < 0) {
+      particles.splice(i, 1);
     }
   });
 
