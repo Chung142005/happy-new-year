fc980cd338b4b925e3b191cb024a2c55e0155349
Chung <cn268471@gmail.com>
2026-02-02 03:07:22 +0700

    code 3:07 ngày 2/2/2026

--- script.js
+++ script.js
@@ -8,28 +8,32 @@ function random(min, max) {
   return Math.random() * (max - min) + min;
 }
 class Particle {
-  constructor(x, y) {
+  constructor(x, y, color) {
     this.x = x;
     this.y = y;
-    this.color = `hsl(${random(0, 360)}, 100%, 75%)`;
-    this.radius = 1.3;
+    // this.color = `hsl(${random(0, 360)}, 100%, 75%)`;
+    this.color = color;
+    this.radius = 4;
 
     const goc = Math.random() * Math.PI * 2;
     const vantoc = Math.random() * 3;
-    this.vantocx = Math.cos(goc) * vantoc;
-    this.vantocy = Math.sin(goc) * vantoc;
+    this.vx = Math.cos(goc) * vantoc;
+    this.vy = Math.sin(goc) * vantoc;
     this.life = 60;
   }
   update() {
-    this.x = this.x + this.vantocx;
-    this.y = this.y + this.vantocy;
-    this.vantocy = this.vantocy + 0.01;
-    this.life--;
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+    this.vy = this.vy + 0.02;
+    this.radius *= 0.98;
+    this.life = this.life - 0.7;
   }
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fillStyle = this.color;
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = this.color;
     ctx.fill();
   }
 }
@@ -41,13 +45,24 @@ function exploide(x, y, color) {
     particles.push(new Particle(x, y, color));
   }
 }
+function explodedCircle(x, y, color) {
+  const count = 60;
+  const speed = 3;
+
+  for (let i = 0; i < 60; i++) {
+    const angle = ((Math.PI * 2) / count) * i;
+
+    const vx = Math.cos(angle) * speed;
+    const vy = Math.sin(angle) * speed;
+  }
+}
 
 class Firework {
   constructor() {
     this.x = random(0, canvas.width);
     this.y = canvas.height;
     this.targetY = random(100, canvas.height / 2);
-    this.radius = 2;
+    this.radius = 4;
     this.color = `hsl(${random(0, 360)}, 100%, 75%)`;
     this.speed = random(4, 7);
     this.exploded = false;
